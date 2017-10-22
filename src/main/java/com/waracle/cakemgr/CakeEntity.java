623cb54f54ed3b253e0c57afa2f68ab4b9ffde3f
grahammartin <graham.martin@sky.uk>
2017-10-22 17:00:59 +0100

    renaming database fields to applicable values

--- src/main/java/com/waracle/cakemgr/CakeEntity.java
+++ src/main/java/com/waracle/cakemgr/CakeEntity.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @Entity
 @org.hibernate.annotations.Entity(dynamicUpdate = true)
-@Table(name = "Employee", uniqueConstraints = {@UniqueConstraint(columnNames = "ID"), @UniqueConstraint(columnNames = "EMAIL")})
+@Table(name = "Cake", uniqueConstraints = {@UniqueConstraint(columnNames = "ID"), @UniqueConstraint(columnNames = "TITLE")})
 public class CakeEntity implements Serializable {
 
     private static final long serialVersionUID = -1798070786993154676L;
@@ -16,13 +16,13 @@ public class CakeEntity implements Serializable {
     @Column(name = "ID", unique = true, nullable = false)
     private Integer employeeId;
 
-    @Column(name = "EMAIL", unique = true, nullable = false, length = 100)
+    @Column(name = "TITLE", unique = true, nullable = false, length = 100)
     private String title;
 
-    @Column(name = "FIRST_NAME", unique = false, nullable = false, length = 100)
+    @Column(name = "DESCRIPTION", unique = false, nullable = false, length = 100)
     private String description;
 
-    @Column(name = "LAST_NAME", unique = false, nullable = false, length = 300)
+    @Column(name = "IMAGE", unique = false, nullable = false, length = 300)
     private String image;
 
     public String getTitle() {
